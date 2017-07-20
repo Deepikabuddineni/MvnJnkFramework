7cf72174aecc924f300c414ee167ae2f39cb3237
Deepikabuddineni <deeparaob@yahoo.com>
2017-07-20 14:58:13 -0400

    MvnJnkFramework push

---
+++ src/test/java/com/fanniemae/testcases/Dummy_Testcase.java
@@ -0,0 +1,26 @@
+package com.fanniemae.testcases;
+
+import com.fanniemae.base.TestBase;
+
+
+
+public class Dummy_Testcase {
+	
+
+public class SDET_Login_Testcase extends TestBase{
+	
+	
+@Test
+public void tc_loginTestCase(){
+	
+	
+}
+@Test(enabled =false)
+public void tc_NewAccountTestCase(){
+}
+	
+
+}
+
+
+}
---
+++ src/test/java/com/fanniemae/testcases/SDET_Login_Testcase.java
@@ -0,0 +1,38 @@
+package com.fanniemae.testcases;
+
+
+
+
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import com.fanniemae.base.TestBase;
+
+public class SDET_Login_Testcase extends TestBase{
+	
+	
+@Test
+public void tc_loginTestCase(){
+	click("xpath_LoginBtn");
+	type("xpath User","");
+	type("id_pwd","");
+	click ("id_bottomLoginbutton");
+	Assert.assertEquals("","");
+	
+	
+	
+}
+	
+	
+
+
+@Test(enabled =false)
+public void tc_NewAccountTestCase(){
+}
+	
+
+}
